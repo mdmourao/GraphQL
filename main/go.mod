5dc5332920ae657c914d70487c97589924879b3b
Martim Mourao <martimmourao@gmail.com>
2023-06-03 23:52:12 +0100

    Bug fixes

--- main/go.mod
+++ main/go.mod
@@ -5,7 +5,6 @@ go 1.20
 require (
 	github.com/99designs/gqlgen v0.17.31
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/go-chi/chi v1.5.4
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/vektah/gqlparser/v2 v2.5.1
--- main/server.go
+++ main/server.go
@@ -6,11 +6,9 @@ import (
 	"os"
 	"tutorialGo/v2/db"
 	"tutorialGo/v2/graph"
-	"tutorialGo/v2/internal/auth"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/go-chi/chi"
 )
 
 const defaultPort = "8080"
@@ -25,14 +23,10 @@ func main() {
 	defer db.CloseDB()
 	db.Migrate()
 
-	router := chi.NewRouter()
-
-	router.Use(auth.Middleware())
-
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	router.Handle("/query", srv)
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
